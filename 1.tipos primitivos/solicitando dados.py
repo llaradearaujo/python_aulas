7d7962467b36104682057262055e55b5bd07cc95
llaradearaujo <laradearaujo300@gmail.com>
2024-08-20 17:09:36 +0000

    2. estruturas condicionais

---
+++ 1.tipos primitivos/solicitando dados.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls || clear") #limpa o terminal
+
+#solicitando dados
+nome = input("digite seu nome: ") #pedir ao usuario um dado do tipo texto
+idade = int(input("digite sua idade: ")) #pedir dado do tipo numero inteiro
+altura = float(input("digite sua altura: "))#pedir dado do tipo numero real
+
+#exibindo dados
+print(f"nome: {nome}") #f permite as chaves em torno da variavel, chaves servem para mencionar variavel
+print(f"idade: {idade} anos.")
+print(f"idade: {idade}; nome: {nome}")
