6acbea3e77db1f06996f84a3d04b868a011f3f78
llaradearaujo <laradearaujo300@gmail.com>
2024-08-23 16:30:16 +0000

    python

---
+++ python/exercicios/meses do ano.py
@@ -0,0 +1,39 @@
+import os
+os.system("cls || clear")
+
+print("""
+meses do ano:
+1- janeiro
+2- fevereiro
+3- março
+4- abril
+5- maio
+6- junho
+7- julho
+8- agosto
+9- setembro
+10- outrubro
+11- novembro
+12- dezembro
+""")
+
+mes = int(input(" DIGITE O NÚMERO RELACIONADO AO MÊS DESEJADO"))
+
+match(mes)
+case 1:
+print("janeiro")
+case 2:
+print("fevereiro")
+case 3:
+print("março")
+case 4:
+print("abril")
+case 5:
+print("maio")
+case 6:
+print("junho")
+case 7:
+print("julho")
+case 8:
+print("agosto")
+case 9:
