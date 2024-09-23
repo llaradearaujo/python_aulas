10f4a513f05057934a2041097f9c36f3bf7261eb
llaradearaujo <laradearaujo300@gmail.com>
2024-09-23 16:54:05 +0000

    lara

---
+++ python/colaborativo/teste de versão.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls || clear")
+
+print("hello world")
+
+#git log - historicos de salvamentos
+#git status - identificar arquivos para serem salvos
+#git add . - adiciona rquivos para salvamento
+#git commit -m "mensagem"- salva arquivo 
