4fe262c426b2f9e92fad0af0f7a8bbfa10d1d565
llaradearaujo <laradearaujo300@gmail.com>
2024-09-26 17:58:34 +0000

    aula 26/09 parte 1

--- python/comandos/estrutura de dados/classe.py
+++ python/comandos/estrutura de dados/classe.py
@@ -31,3 +31,20 @@ print("EXIBINDO DADOS DE ALUNOS")
 for aluno in lista_alunos:
     print(f"nome = {aluno.nome}")
     print(f"idade = {aluno.idade}")
+
+
+#salvando dados do aluno num  arquivo txt
+
+#definindo arquivo para salvar os dados
+nome_do_arquivo = "lista_alunos_senai.txt"
+
+#abrindo arquivo e definindo que sera feito
+with open(nome_do_arquivo, "w") as arquivo_alunos:
+    
+#percorrendo vetor e escrevendo no arquivo uma linha de cada vez
+    for aluno in lista_alunos:
+        arquivo_alunos.write(f"{aluno.nome}, {aluno.idade}\n")
+
+
+
+print("dados salvos com sucesso!")
